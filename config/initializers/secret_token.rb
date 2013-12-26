660997e5699fb275b89dc2a067504a8cc7a27e51
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-12-26 09:46:53 +0800

    Feddit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Feddit::Application.config.secret_key_base = '95808914d2f0474c2b9f9fcead2d77dbb666aa79e2cf0100fa3b60c2d962685a77f8eef44972c313bd62c5908b7b740328d91f0ebcc60e2db1a9b1220530fff3'
