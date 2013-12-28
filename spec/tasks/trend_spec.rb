676a8be0537a5516220eb8552dd0d7fdbfb87a03
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-12-28 12:23:24 +0800

    Added failing test for fetching the trends. Added the coverage directory to the .gitignore file

--- spec/tasks/trend_spec.rb
+++ spec/tasks/trend_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 
 describe "trend:hot" do
   include_context "rake"
+
+  it "gets all the hot trends" do
+    expect {
+      subject.invoke
+    }.to change(Trend, count).by 1
+  end
 end
