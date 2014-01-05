57f3de2dccf62664fd4a94456688087ba08723e8
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2014-01-05 10:07:20 +0800

    Initialize subreddits

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Feddit::Application.initialize!
+
+
+SUBREDDITS = []
+
+File.open(Rails.root.join("config/subreddits")).read.each_line do |line|
+  SUBREDDITS << line.gsub!("\n", "")
+end
