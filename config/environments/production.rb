de0c9462dfa042469bf575a933f3c37b89b15144
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2014-01-14 21:34:04 +0800

    Changed the default host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Feddit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = {host: "www.feddit.neilmarion.com"}
+  config.action_mailer.default_url_options = {host: "www.fedd.it"}
 
   I18n.enforce_available_locales = false
 
